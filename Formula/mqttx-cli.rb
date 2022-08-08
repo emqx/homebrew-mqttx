50711ae88275e93c65f3bee713eb985405963919
ysfscream <894402575bt@gmail.com>
2022-08-08 10:04:15 +0800

    Update mqttx-cli 1.8.2

--- Formula/mqttx-cli.rb
+++ Formula/mqttx-cli.rb
@@ -1,9 +1,10 @@
 class MqttxCli < Formula
   desc "MQTT 5.0 and MQTT X CLI client"
   homepage "https://mqttx.app"
-  url "https://github.com/emqx/MQTTX/releases/download/v1.8.0/mqttx-cli.tar.gz"
-  sha256 "d9b8db76bb966fa5e9fcdc3b1fc3a17e3302c5dc9cb6f05e20eead814b37241a"
+  url "https://github.com/emqx/MQTTX/releases/download/v1.8.2/mqttx-cli.tar.gz"
+  sha256 "60cdfa74fe4f9fd9bafcf8d552a410ae6a7f432d7b7e9f66d8ce74f3ee5f0b80"
   license "Apache-2.0"
+  version "1.8.2"
 
   def install
     bin.install "mqttx"
