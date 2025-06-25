a61fb354502d9a8e8257a95a31ec5d13d0d5bcd7
YuShifan <894402575bt@gmail.com>
2025-06-25 17:50:25 +0800

    Update mqttx-cli 1.12.0

--- Formula/mqttx-cli.rb
+++ Formula/mqttx-cli.rb
@@ -1,7 +1,7 @@
 class MqttxCli < Formula
   desc "MQTT 5.0 and MQTT X CLI client"
   homepage "https://mqttx.app"
-  url "https://github.com/emqx/MQTTX/releases/download/v1.11.1/mqttx-cli.tar.gz"
+  url "https://github.com/emqx/MQTTX/releases/download/v1.12.0/mqttx-cli.tar.gz"
   sha256 "68e87a2415e42469048a86ba7e501c0583dcbb521d9adfec69a0c476b4c75606"
   license "Apache-2.0"
   version "1.12.0"
